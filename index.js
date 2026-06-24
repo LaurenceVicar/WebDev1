cf9852988f47d7a33a0fe7650a645a307dd74173
Gabriel Ondiek <167845225+LaurenceVicar@users.noreply.github.com>
2026-06-24 09:13:02 +0300

    Turned the book titles into links for more info on the books

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+async function fetchBooks() {
+    const response = await fetch('index.json');
+    const data = await response.json();
+    const bookContainer = document.getElementById('book');
+    bookContainer.innerHTML = data.Books.map(book => `
+        <div class="col-md-4 mb-4">
+            <div class="card h-100">
+                <img src="${book.image}" class="card-img-top" alt="${book.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <p class="card-text">Author: ${book.author}</p>
+                </div>
+            </div>
+        </div>
+        `).join("");
+}
+fetchBooks();
